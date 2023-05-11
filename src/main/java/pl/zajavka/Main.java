0913f8fc619e9ddaff9a19e9188d03639ba73e87
Damian Fatejów <rootyniarz@users.noreply.github.com>
2023-05-11 09:24:08 +0200

    5. Main -> list all beans

--- src/main/java/pl/zajavka/Main.java
+++ src/main/java/pl/zajavka/Main.java
@@ -3,12 +3,18 @@ package pl.zajavka;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
         ApplicationContext context = new AnnotationConfigApplicationContext(ExampleConfigurationClass.class);
 
         System.out.println("Context created");
+
+        //show all beans for context
+        Arrays.asList(context.getBeanDefinitionNames()).forEach(name-> System.out.println(name));
+
         ExampleBean exampleBean = context.getBean("exampleBean", ExampleBean.class);
         exampleBean.exampleMethod();
     }
