08c528c8cb8ea26186e2d97ee45b820b9eb9f2b6
Damian Fatejów <rootyniarz@users.noreply.github.com>
2023-05-12 09:14:14 +0200

    8. Spring scan for beans configurtion using localization of Marker interface

---
+++ src/main/java/pl/zajavka/code/Marker.java
@@ -0,0 +1,5 @@
+package pl.zajavka.code;
+
+public interface Marker {
+    // Marker interface, który służy do oznaczania lokalizacji, w której Spring m szukć Beanów
+}
--- src/main/java/pl/zajavka/configuration/ExampleConfigurationClass.java
+++ src/main/java/pl/zajavka/configuration/ExampleConfigurationClass.java
@@ -2,9 +2,10 @@ package pl.zajavka.configuration;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import pl.zajavka.code.Marker;
 
 @Configuration
-@ComponentScan(basePackages = "pl.zajavka.code")
+@ComponentScan(basePackageClasses = Marker.class)
 public class ExampleConfigurationClass {
 
 
