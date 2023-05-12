cb8354872c73117bea9e63e5023372285d6140b2
Damian Fatejów <rootyniarz@users.noreply.github.com>
2023-05-12 09:18:39 +0200

    9. Spring scan for beans configuration using localization of Main.class... scanning subfolders (packages) too...

--- src/main/java/pl/zajavka/code/Marker.java
+++
@@ -1,5 +0,0 @@
-package pl.zajavka.code;
-
-public interface Marker {
-    // Marker interface, który służy do oznaczania lokalizacji, w której Spring m szukć Beanów
-}
--- src/main/java/pl/zajavka/configuration/ExampleConfigurationClass.java
+++ src/main/java/pl/zajavka/configuration/ExampleConfigurationClass.java
@@ -2,10 +2,10 @@ package pl.zajavka.configuration;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import pl.zajavka.code.Marker;
+import pl.zajavka.Main;
 
 @Configuration
-@ComponentScan(basePackageClasses = Marker.class)
+@ComponentScan(basePackageClasses = Main.class) // dzieki temu Java sknuje w poszukiwniu beanow paczke w ktorej znajduje sie Main oraz paczki (podkatalogi)
 public class ExampleConfigurationClass {
 
 
